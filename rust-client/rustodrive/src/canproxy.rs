7556a7d45a7dc661da1072ed9ca7c21d14f5389d
kalinkochnev <kalin.kochnev@gmail.com>
2022-06-22 15:20:00 -0400

    Added function to register threads to the canproxy

--- rust-client/rustodrive/src/canproxy.rs
+++ rust-client/rustodrive/src/canproxy.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::thread::JoinHandle;
 
 use socketcan::CANSocket;
 
 use crate::messages::{ODriveMessage, ODriveResponse};
 
 pub struct CANProxy {
-    msg_receiver: Receiver<ODriveMessage>,
+    mpsc_channel: (Sender<ODriveMessage>, Receiver<ODriveMessage>),
     thread_connections: HashMap<usize, Sender<ODriveResponse>>,
 
     awaiting_response: Vec<ODriveMessage>,
@@ -14,20 +15,45 @@ pub struct CANProxy {
 }
 
 impl CANProxy {
-    pub fn new(
-        can_device: &str,
-        msg_receiver: Receiver<ODriveMessage>,
-        thread_connections: HashMap<usize, Sender<ODriveResponse>>
-    ) -> Self {
+    pub fn new(can_device: &str) -> Self {
         // Initialize CANSocket
         let socket = CANSocket::open(can_device).expect("Could not open CAN at can1");
 
+        // Define the channel for the proxy here
+        let mpsc_channel = channel::<ODriveMessage>();
+
         Self {
-            msg_receiver,
-            thread_connections,
+            mpsc_channel,
             socket,
+            thread_connections: HashMap::new(),
             awaiting_response: vec![],
         }
+    }
+
+    pub fn register<T>(&mut self, thread: T) -> JoinHandle<()>
+    where
+        T: FnOnce(Sender<ODriveMessage>, Receiver<ODriveResponse>) + std::marker::Send + 'static,
+    {
+        // Thread <--- CANManager sends ODriveResponse
+        let (thread_sender, thread_receiver) = channel::<ODriveResponse>();
+        let send_to_proxy = self.mpsc_channel.0.clone();
+
+        // Give the proxy the ability to read from threads
+        // and send to the threads
+        let mut max_id = match self.get_max_thread_id() {
+            None => 0,
+            Some(val) => val
+        };
+        self.thread_connections.insert(max_id, thread_sender);
+
+        // Give the thread the ability to send to the proxy
+        // and receive from the proxy
+        std::thread::spawn(|| thread(send_to_proxy, thread_receiver))
+    }
+
+    /// Get the max thread ID that is being tracked
+    fn get_max_thread_id(&self) -> Option<usize> {
+        return self.thread_connections.keys().max().cloned();
     }
 
     /// Takes the next message off the channel and sends it to the CAN device
@@ -37,9 +63,7 @@ impl CANProxy {
     fn rcv_from_CAN(&self) {}
 
     /// it attempts to match and messages that are waiting to one that was received
-    fn match_with_waiting(received_msg: &ODriveMessage) {
-
-    }
+    fn match_with_waiting(received_msg: &ODriveMessage) {}
 
     /// get the channel for a particular access to respond to
     fn thread_channel(&self, thread_id: usize) {}
--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -1,45 +1,21 @@
 use std::{
     collections::HashMap,
     sync::mpsc::{channel, Receiver, Sender},
+    thread::JoinHandle,
 };
 
 use rustodrive::{
-    canproxy::CANProxy, messages::ODriveResponse, 
-    odrivegroup::ODriveGroup
+    canproxy::CANProxy,
+    messages::{ODriveMessage, ODriveResponse},
+    odrivegroup::ODriveGroup,
 };
 
-type AxisSenders = HashMap<usize, Sender<ODriveResponse>>;
-type AxisReceivers = HashMap<usize, Receiver<ODriveResponse>>;
-
-const AXIS_IDs: [usize; 5] = [1, 2, 3, 4, 5];
-
-fn gen_thread_comms() -> (AxisSenders, AxisReceivers) {
-    // Create the sender and receiver vecs. The senders to be given to the CANManager and the receivers to the individual threads
-    let (mut thread_senders, mut thread_receivers) = (HashMap::new(), HashMap::new());
-
-    for axis in AXIS_IDs.into_iter() {
-        let (send, receiver) = channel::<ODriveResponse>();
-        thread_senders.insert(axis, send);
-        thread_receivers.insert(axis, receiver);
-    }
-    (thread_senders, thread_receivers)
-}
-
 fn setup_can() {
-    // Thread <--- CANManager sends ODriveResponse
-    let (main_sender, main_receiver) = channel::<ODriveResponse>();
+    let mut can_proxy = CANProxy::new("can1");
 
-    // Thread(s) --> CANManager (accepts ODriveMessage)
-    let (proxy_send, proxy_receive) = channel();
-
-    let proxy_send_clone = proxy_send.clone();
-    let main_thread = std::thread::spawn(move || {
-        let odrive = ODriveGroup::new(&[0, 1], proxy_send_clone, main_receiver );
+    let main_thread = can_proxy.register(|proxy_send, thread_rcv| {
+        let odrive = ODriveGroup::new(&[0, 1], proxy_send, thread_rcv);
     });
-
-    // let (thread_senders, thread_receivers) = gen_thread_comms(AXIS_IDs);
-
-    let can_manager = CANProxy::new("can1", proxy_receive, main_sender);
 }
 
 fn main() {
