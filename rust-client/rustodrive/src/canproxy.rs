886c52cf2a2e1e612665b4807d87937c2488a165
kalinkochnev <kalin.kochnev@gmail.com>
2022-06-26 22:20:10 -0400

    Added read-only and read-write thread register functions to prevent accessing the same resource as write twice

--- rust-client/rustodrive/src/canproxy.rs
+++ rust-client/rustodrive/src/canproxy.rs
@@ -2,20 +2,25 @@ use std::collections::HashMap;
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread::JoinHandle;
 
+use socketcan::CANFrame;
+
 use crate::cansocket::CANSocket;
 use crate::messages::{ODriveMessage, ODriveResponse};
+use crate::threads::{ReadOnlyCANThread, ReadWriteCANThread};
 
 type ThreadConnection = (JoinHandle<()>, Sender<ODriveResponse>);
-
+type ThreadID = &'static str;
 pub enum ProxyError {
-    ThreadFailedJoin
+    ThreadFailedJoin,
+    ThreadDuplicateID,
 }
 
 pub struct CANProxy {
     mpsc_channel: (Sender<ODriveMessage>, Receiver<ODriveMessage>),
-    thread_connections: HashMap<usize, ThreadConnection>,
+    threads: HashMap<ThreadID, ThreadConnection>,
+    rw_thread: Option<ThreadID>, // There can only be one read and write thread at a time. Store the identifier in here
 
-    awaiting_response: Vec<ODriveMessage>,
+    listeners: Vec<ODriveMessage>,
     socket: CANSocket,
 }
 
@@ -30,82 +35,161 @@ impl CANProxy {
         Self {
             mpsc_channel,
             socket,
-            thread_connections: HashMap::new(),
-            awaiting_response: vec![],
+            rw_thread: None,
+            threads: HashMap::new(),
+            listeners: vec![],
+        }
+    }
+    pub fn register_rw<F>(&mut self, thread_name: &'static str, thread_func: F)
+    where
+        F: FnOnce(ReadWriteCANThread) + std::marker::Send + 'static,
+    {
+        match self.rw_thread {
+            Some(thread_id) => {
+                panic!("Cannot register more than 1 thread to have write access to CAN device")
+            }
+            None => {
+                self.rw_thread = Some(thread_name);
+                self.register(thread_name, move |thread_requester, thread_receiver| {
+                    thread_func(ReadWriteCANThread::new(thread_name, thread_requester, thread_receiver))
+                });
+            }
         }
     }
 
-    pub fn register<F>(&mut self, func: F) where F: FnOnce(Sender<ODriveMessage>, Receiver<ODriveResponse>) + std::marker::Send + 'static,
+    pub fn register_ro<F>(&mut self, thread_name: &'static str, thread_func: F)
+    where
+        F: FnOnce(ReadOnlyCANThread) + std::marker::Send + 'static,
     {
-        // Thread <--- CANManager sends ODriveResponse
-        let (thread_sender, thread_receiver) = channel::<ODriveResponse>();
-        let send_to_proxy = self.mpsc_channel.0.clone();
+        self.register(thread_name, move |thread_requester, thread_receiver| {
+            thread_func(ReadOnlyCANThread::new(thread_name, thread_requester, thread_receiver))
+        });
+    }
 
-        // Give the proxy the ability to read from threads
-        // and send to the threads
-        let max_id = match self.max_thread_id() {
-            None => 0,
-            Some(val) => val + 1
-        };
+    fn register<F>(&mut self, thread_name: &'static str, func: F)
+    where
+        F: FnOnce(Sender<ODriveMessage>, Receiver<ODriveResponse>) + std::marker::Send + 'static,
+    {
+        // Check that the thread ID does not exist already
+        if self.threads.contains_key(thread_name) {
+            panic!("Two threads cannot have the same id ({})", thread_name);
+        }
+
+        // Thread <--- CANManager, sends ODriveResponse
+        let (thread_sender, thread_receiver) = channel::<ODriveResponse>();
+        let thread_requester = self.mpsc_channel.0.clone();
 
         // Give the thread the ability to send to the proxy
         // and receive from the proxy
-        let thread_handle = std::thread::spawn(|| func(send_to_proxy, thread_receiver));
-        
+        let thread_handle = std::thread::spawn(move || {
+            func(thread_requester, thread_receiver);
+        });
+
         // Add the thread and keep track of it
-        self.thread_connections.insert(max_id, (thread_handle, thread_sender));
+        self.threads
+            .insert(thread_name, (thread_handle, thread_sender));
     }
 
-    pub fn unregister(&mut self, thread_id: &usize) -> Result<(), ProxyError> {
-        if self.thread_connections.contains_key(thread_id) {
-            let (thread_handle, _sender) = self.thread_connections.remove(thread_id).unwrap();
+    pub fn unregister(&mut self, thread_name: &str) -> Result<(), ProxyError> {
+        if self.threads.contains_key(thread_name) {
+            // unregister from the general thread connections
+            let (thread_handle, _sender) = self.threads.remove(thread_name).unwrap();
             match thread_handle.join() {
                 Err(_e) => return Err(ProxyError::ThreadFailedJoin),
-                Ok(()) => return Ok(())
+                _ => {},
             }
+
+            // If the thread being unregistered is the read-write thread, set it to none
+            match self.rw_thread {
+                Some(rw_thread_name) => {
+                    if rw_thread_name == thread_name {
+                        self.rw_thread = None
+                    }
+                },
+                _ => {}
+            }
+
+            return Ok(())
         } else {
             panic!("Cannot unregister thread ID that doesn't exist")
         }
     }
 
-    /// Get the max thread ID that is being tracked
-    fn max_thread_id(&self) -> Option<usize> {
-        return self.thread_connections.keys().max().cloned();
+    /// Take messages off the channel, send it through CAN, and then add it to awaiting
+    fn send_to_CAN(&mut self) {
+        unimplemented!();
+
+        let receiver = &self.mpsc_channel.1;
+        for msg in receiver.recv() {
+            self.socket.write_frame(&msg.msg.to_can());
+            self.listeners.push(msg);
+        }
     }
 
-    /// Takes the next message off the channel and sends it to the CAN device
-    fn send_to_CAN(&self) {}
-
     /// Receives responses from ODrive
-    fn rcv_from_CAN(&self) {}
+    fn rcv_from_CAN(&self) {
+        unimplemented!()
+    }
 
     /// it attempts to match and messages that are waiting to one that was received
-    fn match_with_waiting(received_msg: &ODriveMessage) {}
+    fn match_listener(&self, received_frame: &CANFrame) -> Option<ODriveMessage> {
+        unimplemented!()
+    }
 
     /// get the channel for a particular access to respond to
-    fn thread_channel(&self, thread_id: usize) {}
+    fn thread_channel(&self, thread_id: usize) {
+        unimplemented!()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::thread::JoinHandle;
-    use crate::cansocket::CANSocket;
     use super::CANProxy;
 
     #[test]
     fn test_register_thread() {
         let mut can_proxy = CANProxy::new("fakecan");
 
-        for i in (0..3) {
-            can_proxy.register(|send_to_proxy, thread_receiver| {})
-        }
-        assert_eq!(can_proxy.thread_connections.len(), 3);
-        
-        can_proxy.unregister(&1);
-        can_proxy.register(|send, receive| {});
+        can_proxy.register_ro("thread 1", |read_write_can| {});
+        can_proxy.register_ro("thread 2", |read_write_can| {});
+        can_proxy.register_rw("thread 3", |read_write_can| {});
 
-        // Check that the max_id = 3 and the length is 3
-        assert_eq!(can_proxy.thread_connections.len(), 3);
-        assert_eq!(can_proxy.max_thread_id().unwrap(), 3);
+        assert_eq!(can_proxy.threads.len(), 3);
+
+        can_proxy.unregister("thread 1");
+        assert_eq!(can_proxy.threads.len(), 2);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_register_duplicate() {
+        let mut can_proxy = CANProxy::new("fakecan");
+
+        can_proxy.register_ro("thread 1", |read_write_can| {});
+        can_proxy.register_ro("thread 1", |read_write_can| {});
+    }
+
+    #[test]
+    fn test_unregister_rw_thread() {
+        let mut can_proxy = CANProxy::new("fakecan");
+        can_proxy.register_rw("thread 1", |read_write_can| {});
+        can_proxy.register_ro("thread 2", |read_can| {});
+
+        assert_ne!(can_proxy.rw_thread, None);
+        assert_eq!(can_proxy.threads.len(), 2);
+
+        can_proxy.unregister("thread 1");
+        can_proxy.unregister("thread 2");
+
+        assert_eq!(can_proxy.rw_thread, None);
+        assert_eq!(can_proxy.threads.len(), 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_register_duplicate_rw_thread() {
+        let mut can_proxy = CANProxy::new("fakecan");
+        can_proxy.register_rw("thread 1", |can_read_write| {});
+        can_proxy.register_rw("thread 2", |can_read_write| {});
     }
 }
