6af2e5ff6a606f70265d2e13262bd25d43a66288
uconnamber <amber.uconn@gmail.com>
2022-07-27 10:49:10 -0400

    Formatting + Added Quit Option
    Co-authored-by: Rany Kamel <rany.kamel@uconn.edu>

--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -1,6 +1,8 @@
 use rustodrive::{
     canproxy::CANProxy,
-    commands::{ControlMode, ControlMode::*, InputMode, InputMode::*, ODriveAxisState::*, WriteComm},
+    commands::{
+        ControlMode, ControlMode::*, InputMode, InputMode::*, ODriveAxisState::*, WriteComm,
+    },
     messages::CANRequest,
     odrivegroup::ODriveGroup,
     threads::ReadWriteCANThread,
@@ -9,7 +11,7 @@ use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::{error::Error, io::stdin};
 
 fn odrive_main(can_read_write: ReadWriteCANThread) {
-    fn input(txt: &'static str) -> String {
+    fn input(txt: &str) -> String {
         let mut out = String::new();
         println!("{}", txt);
         stdin().read_line(&mut out).unwrap();
@@ -23,9 +25,12 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
     while true {
         //println!();
         //stdin().read_line(&mut inp).unwrap();
-        inp = input("Input (C - Toggle Closed Loop, V - Input Velocity, P - Input Position, CM - Control Mode / Input Mode) > ");
+        let disp_txt = format!("Input (C - Toggle Closed Loop ({}), V - Input Velocity, P - Input Position, CM - Control Mode / Input Mode) > ", is_closed_loop);
+        inp = input(disp_txt.as_str());
         //println!("{}", inp.to_uppercase());
         match inp.to_uppercase().as_str() {
+
+            // Toggle Control Mode
             "C" => {
                 if (is_closed_loop) {
                     odrives.all_axes(|ax| ax.set_state(Idle));
@@ -35,7 +40,8 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
                     is_closed_loop = true;
                 }
             }
-
+            
+            // Change Input Velocity
             "V" => {
                 let inp_vel: f32 = input("Input Velocity > ")
                     .parse::<f32>()
@@ -43,11 +49,13 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
                 odrives.all_axes(|ax| ax.motor.set_input_vel(inp_vel));
             }
 
+            // Change Input Position
             "P" => {
                 let inp_pos: f32 = input("Input Velocity > ").parse::<f32>().unwrap();
                 odrives.all_axes(|ax| ax.motor.set_input_pos(inp_pos));
             }
 
+            // Change Control Mode & Input Mode
             "CM" => {
                 let inp_cm: u32 =
                     input("Input Control Mode (2 - Velocity Control, 3 - Position Control)")
@@ -57,17 +65,27 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
                     input("Input Control Mode (1 - Passthrough, 2 - VelRamp, 3 - PosFilter)")
                         .parse::<u32>()
                         .unwrap_or_default();
-                
-                odrives.all_axes(move |ax| {
-                    let CM = TryInto::<ControlMode>::try_into(inp_cm)
-                        .unwrap_or(ControlMode::VelocityControl);
-                    let IM = TryInto::<InputMode>::try_into(inp_im)
-                        .unwrap_or(InputMode::Passthrough);
-                    ax.motor.set_control_mode(CM, IM)
-                });}
 
+                odrives.all_axes(move |ax| {
+                    let CM = 
+                        TryInto::<ControlMode>::try_into(inp_cm).unwrap_or(ControlMode::VelocityControl);
+                    let IM =
+                        TryInto::<InputMode>::try_into(inp_im).unwrap_or(InputMode::Passthrough);
+                    ax.motor.set_control_mode(CM, IM)
+                });
+            }
+
+            // Quit
+            "Q" => {
+                println!("Quitting...");
+                odrives.all_axes(|ax| ax.set_state(Idle));
+                std::process::exit(0);
+            }
+
+            // Invalid Command Handler
             _ => println!("Not a valid command."),
-    }}
+        }
+    }
     //odrives.all_axes(|ax| ax.set_state(ClosedLoop));
 
     //odrives.all_axes(|ax| ax.motor.set_control_mode(PositionControl));
