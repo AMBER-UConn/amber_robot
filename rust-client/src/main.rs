baf1113174370563c0852aa9f395fdfd5d79ca64
kalinkochnev <kalin.kochnev@gmail.com>
2022-08-02 21:18:24 -0400

    Minimal working imgui setup

--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -5,9 +5,10 @@ use rustodrive::{
     threads::ReadWriteCANThread,
 };
 use signal_hook::{consts::SIGINT, iterator::Signals};
+use ui_test::ui_main;
 use std::{error::Error};
 
-pub mod test_ui;
+pub mod ui_test;
 
 fn init_motors(odrv: &ODriveGroup) {
     odrv.all_axes(|ax| ax.set_state(EncoderIndexSearch));
@@ -15,33 +16,21 @@ fn init_motors(odrv: &ODriveGroup) {
 
 fn odrive_main(can_read_write: ReadWriteCANThread) {
     let odrives = ODriveGroup::new(can_read_write, &[0, 1, 2, 3, 4, 5]);
-
-    init_motors(&odrives);
-
-    // test_ui::ui_start(odrives);
-
-
-    //odrives.all_axes(|ax| ax.set_state(ClosedLoop));
-
-    odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::PositionControl, InputMode::PosFilter));
-    //odrives.all_axes(|ax| ax.motor.set_input_pos(180 as f32 / 360 as f32));
-    //odrives.all_axes(|ax| ax.motor.set_input_vel(10.0));
-    odrives.axis(&0, |ax| ax.set_state(Idle)).unwrap();
-
-    //InputVel when [1; 8]: [1.40e-45, 3.59e-43, 9.18e-41, 2.35e-38, 0, 0, 0, 0]
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut can_proxy = CANProxy::new("can0");
-    can_proxy.register_rw("thread 1", odrive_main);
-    let stop_all = can_proxy.begin();
+    // let mut can_proxy = CANProxy::new("can0");
+    // can_proxy.register_rw("thread 1", ui_main);
+    // let stop_all = can_proxy.begin();
+
+    ui_main();
 
     let mut signals = Signals::new(&[SIGINT])?;
     for sig in signals.forever() {
         println!("\nQuitting the program {:?}", sig);
         break;
     }
-    stop_all().unwrap();
+    // stop_all().unwrap();
     println!("all done!");
     Ok(())
 }
