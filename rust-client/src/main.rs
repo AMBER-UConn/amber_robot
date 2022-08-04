556edb0761423176c72b47d85ad716904ed038eb
kalinkochnev <kalin.kochnev@gmail.com>
2022-08-04 19:25:43 -0400

    Removed non-pertitent stuff from main

--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -1,25 +1,7 @@
-use rustodrive::{
-    canproxy::CANProxy,
-    state::{ODriveAxisState::*, ControlMode, InputMode},
-    odrivegroup::ODriveGroup,
-    threads::ReadWriteCANThread,
-};
 use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::{error::Error};
 
-fn init_motors(odrv: &ODriveGroup) {
-    odrv.all_axes(|ax| ax.set_state(EncoderIndexSearch));
-}
-
-fn odrive_main(can_read_write: ReadWriteCANThread) {
-    let odrives = ODriveGroup::new(can_read_write, &[0, 1, 2, 3, 4, 5]);
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
-    // let mut can_proxy = CANProxy::new("can0");
-    // can_proxy.register_rw("thread 1", ui_main);
-    // let stop_all = can_proxy.begin();
-
     rustodrive_gui::ui_test::ui_main();
 
     let mut signals = Signals::new(&[SIGINT])?;
@@ -27,7 +9,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("\nQuitting the program {:?}", sig);
         break;
     }
-    // stop_all().unwrap();
+
     println!("all done!");
     Ok(())
 }
