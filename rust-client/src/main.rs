cc6784efe90c7e78dc77fe41d34bb17edc79a285
kalinkochnev <kalin.kochnev@gmail.com>
2022-08-08 18:19:07 -0400

    Updated API to use generic conversions to handle data received from CAN due to not knowing at runtime.
    Co-authored-by: Rany Kamel <rany.kamel@uconn.edu>

--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -1,8 +1,7 @@
 use rustodrive::{
     canproxy::CANProxy,
-    state::{ODriveAxisState::*, ControlMode, InputMode},
     odrivegroup::ODriveGroup,
-    threads::ReadWriteCANThread,
+    threads::ReadWriteCANThread, casts::Heartbeat, response::{Success}, utils::ResultAll,
 };
 use signal_hook::{consts::SIGINT, iterator::Signals};
 use std::{error::Error};
@@ -10,10 +9,12 @@ use std::{error::Error};
 pub mod test_ui;
 
 fn init_motors(odrv: &ODriveGroup) {
-    odrv.all_axes(|ax| ax.set_state(EncoderIndexSearch));
+    //odrv.all_axes(|ax| ax.set_state(EncoderIndexSearch));
     std::thread::sleep_ms(2000);
 }
 
+// TODO add remaining panic tests for type conversions
+// TODO update documentation to reflect new changes
 fn odrive_main(can_read_write: ReadWriteCANThread) {
     let odrives = ODriveGroup::new(can_read_write, &[0, 1, 2, 3, 4, 5]);
 
@@ -22,11 +23,14 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
     // test_ui::ui_start(odrives);
 
 
-    odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::VelocityControl, InputMode::VelRamp));
-    odrives.all_axes(|ax| ax.set_state(ClosedLoop));
+    // odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::VelocityControl, InputMode::VelRamp));
+    // odrives.all_axes(|ax| ax.set_state(ClosedLoop));
     //odrives.all_axes(|ax| ax.motor.set_input_pos(180 as f32 / 360 as f32));
-    odrives.all_axes(|ax| ax.motor.set_input_vel(10.0));
+    // odrives.all_axes(|ax| ax.motor.set_input_vel(10.0));
     //odrives.axis(&0, |ax| ax.set_state(Idle)).unwrap();
+    
+    let heartbeat: Vec<Success<Heartbeat>> = odrives.all_axes(|ax| ax.get_heartbeat()).unwrap_all();
+    let heartbeat: Success<Heartbeat> = odrives.axis(&1, |ax| ax.get_heartbeat()).unwrap();
 
     //InputVel when [1; 8]: [1.40e-45, 3.59e-43, 9.18e-41, 2.35e-38, 0, 0, 0, 0]
 }
