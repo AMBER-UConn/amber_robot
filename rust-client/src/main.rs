754de6d916c2c505150e533a9b7600fa5402e2a4
kalinkochnev <kalin.kochnev@gmail.com>
2022-08-09 16:22:27 -0400

    Merged rustodrive/devel into main

--- rust-client/src/main.rs
+++ rust-client/src/main.rs
@@ -13,7 +13,6 @@ fn init_motors(odrv: &ODriveGroup) {
     std::thread::sleep_ms(2000);
 }
 
-// TODO add remaining panic tests for type conversions
 // TODO update documentation to reflect new changes
 fn odrive_main(can_read_write: ReadWriteCANThread) {
     let odrives = ODriveGroup::new(can_read_write, &[0, 1, 2, 3, 4, 5]);
@@ -21,13 +20,11 @@ fn odrive_main(can_read_write: ReadWriteCANThread) {
     init_motors(&odrives);
 
     // test_ui::ui_start(odrives);
-
-
     // odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::VelocityControl, InputMode::VelRamp));
     // odrives.all_axes(|ax| ax.set_state(ClosedLoop));
-    //odrives.all_axes(|ax| ax.motor.set_input_pos(180 as f32 / 360 as f32));
+    // odrives.all_axes(|ax| ax.motor.set_input_pos(180 as f32 / 360 as f32));
     // odrives.all_axes(|ax| ax.motor.set_input_vel(10.0));
-    //odrives.axis(&0, |ax| ax.set_state(Idle)).unwrap();
+    // odrives.axis(&0, |ax| ax.set_state(Idle)).unwrap();
     
     let heartbeat: Vec<Success<Heartbeat>> = odrives.all_axes(|ax| ax.get_heartbeat()).unwrap_all();
     let heartbeat: Success<Heartbeat> = odrives.axis(&1, |ax| ax.get_heartbeat()).unwrap();
