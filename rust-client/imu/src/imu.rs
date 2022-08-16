aaae9e192e0c6b53be995a7a0433939088b09960
Rany Kamel <rany.kamel47@gmail.com>
2022-08-16 17:15:40 -0400

    Merge + Moved checksum code to request + refactored

--- rust-client/imu/src/imu.rs
+++ rust-client/imu/src/imu.rs
@@ -24,13 +24,24 @@ impl IMU {
         }
     }
 
+
     fn request(&mut self, buffer: &[u8]) -> [u8; 11] {
         // Requests buffer info from imu and writes it to output variable
         self.port.write(buffer).expect("Failed to write!");
         //let _ = twos_complement();
         let mut output = [0u8;11];
-        self.port.read_exact(&mut output).expect("Failed to read!");
-        assert!([output[1]] == *buffer, "Checksum failed!");
+        while [output[1]] != *buffer {
+            self.port.read_exact(&mut output).expect("Failed to read!");
+        }
+        println!("{:?}", output);
+         //println!("{:?}", output.split_last().unwrap().1.iter().map(|x| *x as u32).sum::<u32>().to_be_bytes());
+
+        // 85 dec = 0x55 hex
+        assert!(output[0] == 0x55 && 
+                output.split_last().unwrap().1
+                      .iter().map(|x| *x as u32)
+                      .sum::<u32>()
+                      .to_be_bytes()[3] == output[10], "Checksum failed!");
         return output;
     }
 
@@ -40,51 +51,28 @@ impl IMU {
 
         // println!("{}", ((output[4] as u16) << 8) | (output[3] as u16));
 
-        // Compares checksum to make sure no errors occured
-        if output[1] == 0x51 {
-            println!("{:?}", output);
-            let mut calc: i32 = -1 * output[10] as i32;
-            for val in output.iter() {
-                calc += *val as i32;
-            }
-                
-            println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
-
             // Prints out accX accY accZ for our viewing pleasure
             for i in (  2..output.len() - 2 - 1).step_by(2) {
-                let (accL, accH) = (output[i] as u16, output[i + 1] as u16);
-                println!("{:?}, ", (((accH << 8) | accL) as f32 /32768.0 * 16.0 * 9.81));
+                let (accL, accH) = (output[i] as i32, output[i + 1] as i32);
+                print!("{:?}, ", (((accH << 8) | accL) as f32 / 32768.0 * 16.0 * 9.81));
             }
             // for (i, acc) in &output[2..-2].chunks(2).enumerate() {
             //     let (accL, accH) = (acc[0] as u16, acc[1] as u16);
             //     print!("{:?}, ", (((accH << 8) | accL) as f32 /32768.0 * 16.0));
             // }
         println!("\n");
-        }
-        
     }
 
     pub fn get_ang_vel(&mut self) {
         // Requests angular velocity info from imu and writes it to output variable
         let output = self.request(&[0x52]);
 
-        // Compares checksum to make sure no errors occured
-        if output[1] == 0x52 {
-            println!("{:?}", output);
-            let mut calc: i32 = -1 * output[10] as i32;
-            for val in output.iter() {
-                calc += *val as i32;
-            }
-
-            println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
-
             // Prints out wX wY wX for our viewing pleasure
             for i in (2..output.len()-2-1).step_by(2) {
-                let (velL, velH) = (output[i] as u16, output[i+1] as u16);
-                println!("{:?} ", (((velH<<8)|velL)/32768*2000))
+                let (velL, velH) = (output[i] as i32, output[i+1] as i32);
+                print!("{:?} ", (((velH<<8)|velL) as f32 / 32768.0 * 2000.0))
             }
         println!("\n");
-        }
     }
 
     pub fn get_ang(&mut self) {
@@ -92,23 +80,16 @@ impl IMU {
         let output = self.request(&[0x53]);
 
         // Compares checksum to make sure no errors occured
-        if output[1] == 0x53 {
-            let mut calc: i32 = -1 * output[10] as i32;
-            for val in output.iter() {
-                calc += *val as i32;
-            }
-
-            println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
 
             // Prints out ROLL PITCH YAW (degrees) for our viewing pleasure
             for i in (2..output.len()-2-1).step_by(2) {
-                let (angL, angH) = (output[i] as u16, output[i+1] as u16);
-                println!("{:?} ", (((angH<<8)|angL)/32768*180))
+                let (angL, angH) = (output[i] as i32, output[i+1] as i32);
+                print!("{:?} ", (((angH<<8)|angL) as f32 / 32768.0 * 180.0))
             }
         println!("\n");
-        }
     }
 }
+
 
 
 pub fn test() {
@@ -124,11 +105,14 @@ pub fn test() {
         // port.read(serial_buf.as_mut_slice())
         //     .expect("Found no data!");
         // println!("{:?}", serial_buf);
+
         println!("ACCELERATION:");
         imu.get_acc();
         println!("ANGULAR VELOCITY:");
         imu.get_ang_vel();
         println!("ANGLE (ROLL PITCH YAW):");
         imu.get_ang();
+
+
     }
 }
