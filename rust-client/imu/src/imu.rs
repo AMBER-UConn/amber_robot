02e52f4d0c1d50a0c391ef6730322cd3bbb14149
amber-uconn <amber.uconn@gmail.com>
2022-08-16 12:27:51 -0400

    Added angle and angular velocity support

--- rust-client/imu/src/imu.rs
+++ rust-client/imu/src/imu.rs
@@ -14,7 +14,12 @@ pub fn test() {
         // port.read(serial_buf.as_mut_slice())
         //     .expect("Found no data!");
         // println!("{:?}", serial_buf);
-        get_acc(&mut port)
+        println!("ACCELERATION:");
+        get_acc(&mut port);
+        println!("ANGULAR VELOCITY:");
+        get_ang_vel(&mut port);
+        println!("ANGLE (ROLL PITCH YAW):");
+        get_ang(&mut port);
     }
 }
 
@@ -22,12 +27,15 @@ fn twos_complement() -> i32 {
     1
 }
 fn get_acc(port: &mut Box<dyn SerialPort>) {
+    // Requests acceleration info from imu and writes it to output variable
     port.write(&[0x51]).expect("Failed to write");
     let _ = twos_complement();
     let mut output = [0u8;11];
     port.read_exact(&mut output);
 
     // println!("{}", ((output[4] as u16) << 8) | (output[3] as u16));
+
+    // Compares checksum to make sure no errors occured
     if output[1] == 0x51 {
         println!("{:?}", output);
         let mut calc: i32 = -1 * output[10] as i32;
@@ -37,6 +45,7 @@ fn get_acc(port: &mut Box<dyn SerialPort>) {
             
         println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
 
+        // Prints out accX accY accZ for our viewing pleasure
         for i in (  2..output.len() - 2 - 1).step_by(2) {
             let (accL, accH) = (output[i] as u16, output[i + 1] as u16);
             print!("{:?}, ", (((accH << 8) | accL) as f32 /32768.0 * 16.0 * 9.81));
@@ -48,4 +57,56 @@ fn get_acc(port: &mut Box<dyn SerialPort>) {
     println!("\n");
     }
     
+}
+
+fn get_ang_vel(port: &mut Box<dyn SerialPort>) {
+    // Requests angular velocity info from imu and writes it to output variable
+    port.write(&[0x52]).expect("Failed to write");
+    let _ = twos_complement();
+    let mut output = [0u8; 11];
+    port.read_exact(&mut output);
+
+    // Compares checksum to make sure no errors occured
+    if output[1] == 0x52 {
+        println!("{:?}", output);
+        let mut calc: i32 = -1 * output[10] as i32;
+        for val in output.iter() {
+            calc += *val as i32;
+        }
+
+        println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
+
+        // Prints out wX wY wX for our viewing pleasure
+        for i in (2..output.len()-2-1).step_by(2) {
+            let (velL, velH) = (output[i] as u16, output[i+1] as u16);
+            print!("{:?} ", (((velH<<8)|velL)/32768*2000))
+        }
+    println!("\n");
+    }
+}
+
+fn get_ang(port: &mut Box<dyn SerialPort>) {
+    // Requests angle info from imu and writes it to output variable
+    port.write(&[0x53]).expect("Failed to write");
+    let _ = twos_complement();
+    let mut output = [0u8; 11];
+    port.read_exact(&mut output);
+
+    // Compares checksum to make sure no errors occured
+    if output[1] == 0x53 {
+        println!("{:?}", output);
+        let mut calc: i32 = -1 * output[10] as i32;
+        for val in output.iter() {
+            calc += *val as i32;
+        }
+
+        println!("calculated: {} ---- actual: {}", calc >> 2, output[10]);
+
+        // Prints out ROLL PITCH YAW (degrees) for our viewing pleasure
+        for i in (2..output.len()-2-1).step_by(2) {
+            let (angL, angH) = (output[i] as u16, output[i+1] as u16);
+            print!("{:?} ", (((angH<<8)|angL)/32768*180))
+        }
+    println!("\n");
+    }
 }
