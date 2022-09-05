46d19f30fdffa87459c83c80a3ffeeffeb6f40ac
amber-uconn <amber.uconn@gmail.com>
2022-09-05 13:43:18 -0400

    Controller stuff
    Co-authored-by: Anna Vladimirskaya <anna.vladimirskaya@uconn.edu>
    Co-authored-by: Rany Kamel <rany.kamel@uconn.edu>

--- rust-client/src/controller.rs
+++ rust-client/src/controller.rs
@@ -1,12 +1,12 @@
 use gilrs::{Gilrs, Button, Event};
-use rustodrive::{odrivegroup::ODriveGroup, state::{AxisState::*, ControlMode, InputMode}}
+use rustodrive::{odrivegroup::ODriveGroup, state::{AxisState::*, ControlMode, InputMode}, response::Success, utils::ResultAll};
 
 //pub mod gamepad;
 
-fn main() {
+pub fn controller(odrives: ODriveGroup) {
 
 
-    let odrv = ODriveGroup::new(can_read_WRITE, &[0,1]);
+    //let odrv = ODriveGroup::new(can_read_WRITE, &[0,1]);
 
     let mut gilrs = Gilrs::new().unwrap();
     
@@ -14,7 +14,7 @@ fn main() {
     for (_id, gamepad) in gilrs.gamepads() {
         println!("{} is {:?}", gamepad.name(), gamepad.power_info());
     }
-    
+    let mut speed = 0.0;
 
     let mut active_gamepad = None;
     
@@ -30,13 +30,19 @@ fn main() {
                 println!("Button South is pressed (XBox - A, PS - X)");
             }
             if gamepad.is_pressed(Button::North){
-                println!("Button North is pressed");
-                odrv.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
-                odrv.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
-
+                println!("Closed Loop!");
+                let aaa: Vec<Success<()>> = odrives.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
             }
 
-            
-        }
+            if gamepad.is_pressed(Button::DPadUp) {
+                let b: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_input_vel(10.0)).unwrap_all();
+            }
+
+            if gamepad.is_pressed(Button::DPadDown) {
+                let b: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_input_vel(-10.0)).unwrap_all();
+            }
+
+
     }
+}
 }
