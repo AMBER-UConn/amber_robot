8e8eaff60fe0ef7d90e145cfbe393a35014a89aa
amber-uconn <amber.uconn@gmail.com>
2022-09-05 12:47:08 -0400

    some stuff
    Co-authored-by: Anna Vladimirskaya <anna.vladimirskaya@uconn.edu>
    Co-authored-by: Rany Kamel <rany.kamel@uconn.edu>

---
+++ rust-client/src/controller.rs
@@ -0,0 +1,42 @@
+use gilrs::{Gilrs, Button, Event};
+use rustodrive::{odrivegroup::ODriveGroup, state::{AxisState::*, ControlMode, InputMode}}
+
+//pub mod gamepad;
+
+fn main() {
+
+
+    let odrv = ODriveGroup::new(can_read_WRITE, &[0,1]);
+
+    let mut gilrs = Gilrs::new().unwrap();
+    
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+        println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+    
+
+    let mut active_gamepad = None;
+    
+    loop {
+        // Examine new events
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            active_gamepad = Some(id);
+        }
+
+        // You can also use cached gamepad state
+        if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            if gamepad.is_pressed(Button::South) {
+                println!("Button South is pressed (XBox - A, PS - X)");
+            }
+            if gamepad.is_pressed(Button::North){
+                println!("Button North is pressed");
+                odrv.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
+                odrv.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
+
+            }
+
+            
+        }
+    }
+}
