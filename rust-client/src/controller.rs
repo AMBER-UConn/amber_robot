3469fa059f5ff3d22a4762184fafd522187bbad7
amber-uconn <amber.uconn@gmail.com>
2022-09-06 15:22:39 -0400

    added position control/velo control toggle and input pos
    Co-authored-by: Anna Vladimirskaya <anna.vladimirskaya@uconn.edu>
    Co-authored-by: Rany Kamel <rany.kamel@uconn.edu>

--- rust-client/src/controller.rs
+++ rust-client/src/controller.rs
@@ -31,60 +31,94 @@ pub fn controller(odrives: ODriveGroup) {
         // You can also use cached gamepad state
         if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
 
+            let mut is_vel = true;
+
             fn check_neg(a: f32) -> bool {
                 return (a == a.abs()).not();
             }
 
-            let ls_x = gamepad.value(Axis::LeftStickX);
-            let ls_y = gamepad.value(Axis::LeftStickY);
-            let mut ls_rad = (ls_y / ls_x).atan();
+            fn set_speed(odrives: &ODriveGroup, sp: f32) -> () {
+                let b: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_input_vel(sp)).unwrap_all();
+            }
+
+            fn set_pos(odrives: &ODriveGroup, po: f32) -> () {
+                let b: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_input_pos(po)).unwrap_all();
+            }
+
+
+
+            if gamepad.is_pressed(Button::South) { //pos
+                let aaa: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::PositionControl, InputMode::PosFilter)).unwrap_all();
+                println!("POSITION CONTROL!");
+                is_vel = false;
+            }
+
+            if gamepad.is_pressed(Button::West) { //vel
+                let aaa: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_control_mode(ControlMode::VelocityControl, InputMode::VelRamp)).unwrap_all();
+                println!("VELOCITY CONTROL!");
+                is_vel = true;
+            }
             
-
-            if check_neg(ls_x) && check_neg(ls_y) { //3rd q
-                ls_rad += PI;
-            }
-            else if check_neg(ls_x) { //2nd q
-                ls_rad += PI;
-            }
-            else if check_neg(ls_y) { //4th q
-                ls_rad += 2.0*PI;
-            }
-
-            let mut ls_deg = ls_rad * (180.0/PI);
-
-            println!("LS: {}", ls_deg);  
-
-            let mut ls_rot = ls_deg / 360.0;
-
-            if gamepad.is_pressed(Button::South) {
-                println!("Button South is pressed (XBox - A, PS - X)");
-            }
+            
             if gamepad.is_pressed(Button::North){
                 println!("Closed Loop!");
                 let aaa: Vec<Success<()>> = odrives.all_axes(|ax| ax.set_state(ClosedLoop)).unwrap_all();
             }
-
-            if gamepad.is_pressed(Button::DPadUp) {
-                speed = speed.abs();
-            }
-
-            if gamepad.is_pressed(Button::DPadDown) {
-                speed = -speed.abs();
-            }
             
-            if gamepad.is_pressed(Button::DPadRight) {
-                speed = speed + (1.0/60.0);
+
+            if is_vel {
+                if gamepad.is_pressed(Button::DPadUp) {
+                    speed = speed.abs();
+                    set_speed(&odrives, speed);
+                }
+
+                if gamepad.is_pressed(Button::DPadDown) {
+                    speed = -speed.abs();
+                    set_speed(&odrives, speed);
+                }
+                
+                if gamepad.is_pressed(Button::DPadRight) {
+                    speed = speed + (1.0/60.0);
+                    set_speed(&odrives, speed);
+                }
+                
+                if gamepad.is_pressed(Button::DPadLeft) {
+                    speed = speed - (1.0/60.0);
+                    set_speed(&odrives, speed);
+                }
+
+                if gamepad.is_pressed(Button::East) {
+                    speed = 0.0;
+                    set_speed(&odrives, speed);
+                }
             }
+
+            //if !is_vel {
+                let ls_x = gamepad.value(Axis::LeftStickX);
+                let ls_y = gamepad.value(Axis::LeftStickY);
+                let mut ls_rad = (ls_y / ls_x).atan();
+                
+
+                if check_neg(ls_x) && check_neg(ls_y) { //3rd q
+                    ls_rad += PI;
+                }
+                else if check_neg(ls_x) { //2nd q
+                    ls_rad += PI;
+                }
+                else if check_neg(ls_y) { //4th q
+                    ls_rad += 2.0*PI;
+                }
+
+                let mut ls_deg = ls_rad * (180.0/PI);
+
+                println!("LS: {}   {}", ls_deg, ls_x);  
+
+                let mut ls_rot = ls_deg / 360.0;
+
+                set_pos(&odrives, ls_rot);
+            //}
             
-            if gamepad.is_pressed(Button::DPadLeft) {
-                speed = speed - (1.0/60.0);
-            }
-
-            if gamepad.is_pressed(Button::East) {
-                speed = 0.0;
-            }
-
-            let b: Vec<Success<()>> = odrives.all_axes(|ax| ax.motor.set_input_vel(speed)).unwrap_all();
+            
                       
 
     }
